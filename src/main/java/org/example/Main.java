1d1662f97625baed07ffb5ae07a87bbdcdcc1f8f
Denis Stepshin <Evil9493@ya.ru>
2023-01-21 22:48:12 +0600

    added method print() invocation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,6 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
 
@@ -16,5 +14,7 @@ public class Main {
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+
+        Utilities.print();
     }
 }
